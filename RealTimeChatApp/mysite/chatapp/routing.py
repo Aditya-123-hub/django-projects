75ee281591569ca99c58dd2916b26e1f5560966c
Aditya-123-hub <aditya.22210362@viit.ac.in>
2025-09-18 18:01:12 +0530

    fixed routing issue

--- RealTimeChatApp/mysite/chatapp/routing.py
+++ RealTimeChatApp/mysite/chatapp/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .import consumers
+from django.urls import re_path
+from . import consumers
 
-websocket_urlpatterns=[
-    path('ws/<str:room_name>/',consumers.ChatConsumer.as_asgi()),
+websocket_urlpatterns = [
+    re_path(r'ws/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
